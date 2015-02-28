68ad8c3248833f43282193d8b58cead4f7dfa1cb
Reece Webb <inbox@reece.org>
2015-02-27 17:54:47 -0800

    Table View controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Reece Webb on 2/27/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
