f9922fb7d848bd0eea7048e4dccebb2ee3a8fb32
Reece Webb <inbox@reece.org>
2015-04-09 18:41:38 -0700

    Retry Image Downloads

--- Blocstagram/MediaTableViewCell.m
+++ Blocstagram/MediaTableViewCell.m
@@ -165,7 +165,7 @@ static NSParagraphStyle *paragraphStyle;
     if (_mediaItem.image) {
         self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
     } else {
-        self.imageHeightConstraint.constant = 0;
+        self.imageHeightConstraint.constant = 150;
     }
     
     // Hide the line between cells
