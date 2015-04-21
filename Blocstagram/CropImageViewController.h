62aa2a910cbb1c6b9784663672d84821db7509e6
Reece Webb <inbox@reece.org>
2015-04-20 18:14:24 -0700

    Added Photo Library and Crop View

---
+++ Blocstagram/CropImageViewController.h
@@ -0,0 +1,26 @@
+//
+//  CropImageViewController.h
+//  Blocstagram
+//
+//  Created by Reece Webb on 4/20/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MediaFullScreenViewController.h"
+
+@class CropImageViewController;
+
+@protocol CropImageViewControllerDelegate <NSObject>
+
+- (void) cropControllerFinishedWithImage:(UIImage *)croppedImage;
+
+@end
+
+@interface CropImageViewController : MediaFullScreenViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@property (nonatomic, weak) NSObject <CropImageViewControllerDelegate> *delegate;
+
+@end
---
+++ Blocstagram/CropImageViewController.m
@@ -0,0 +1,141 @@
+//
+//  CropImageViewController.m
+//  Blocstagram
+//
+//  Created by Reece Webb on 4/20/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "CropImageViewController.h"
+#import "CropBox.h"
+#import "Media.h"
+#import "UIImage+ImageUtilities.h"
+#import "CameraToolbar.h"
+#import "CameraViewController.h"
+
+@interface CropImageViewController () <CameraToolbarDelegate>
+
+@property (nonatomic, strong) CropBox *cropBox;
+@property (nonatomic, assign) BOOL hasLoadedOnce;
+@property (nonatomic, strong) CameraToolbar *cameraToolbar;
+@property (nonatomic, strong) UIToolbar *topView;
+@property (nonatomic, strong) UIToolbar *bottomView;
+
+@end
+
+@implementation CropImageViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage {
+    self = [super init];
+    
+    if (self) {
+        self.media = [[Media alloc] init];
+        self.media.image = sourceImage;
+        
+        self.cropBox = [CropBox new];
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.clipsToBounds = YES;
+    
+    [self.view addSubview:self.cropBox];
+    
+    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Crop", @"Crop command") style:UIBarButtonItemStyleDone target:self action:@selector(cropPressed:)];
+    
+    self.navigationItem.title = NSLocalizedString(@"Crop Image", nil]);
+    self.navigationItem.rightBarButtonItem = rightButton;
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    self.view.backgroundColor = [UIColor colorWithWhite:0.8 alpha:1];
+    UIColor *whiteBG = [UIColor colorWithWhite:1.0 alpha:.15];
+    
+    self.topView.barTintColor = whiteBG;
+    self.bottomView.barTintColor = whiteBG;
+    self.topView.alpha = 0.2;
+    self.bottomView.alpha = 1;
+    
+    [self.view addSubview:self.topView];
+    [self.view addSubview:self.cropBox];
+    [self.view addSubview:self.bottomView];
+    [self.view addSubview:self.cameraToolbar];
+}
+
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    CGRect cropRect = CGRectZero;
+    
+    CGFloat edgeSize = MIN(CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+    cropRect.size = CGSizeMake(edgeSize, edgeSize);
+    
+    CGSize size = self.view.frame.size;
+    
+    self.cropBox.frame = cropRect;
+    self.cropBox.center = CGPointMake(size.width / 2, size.height / 2);
+    self.scrollView.frame = self.cropBox.frame;
+    self.scrollView.clipsToBounds = NO;
+    
+    [self recalculateZoomScale];
+    
+    if (self.hasLoadedOnce == NO) {
+        self.scrollView.zoomScale = self.scrollView.minimumZoomScale;
+        self.hasLoadedOnce = YES;
+    }
+    
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    self.topView.frame = CGRectMake(0, self.topLayoutGuide.length, width, 44);
+    
+    CGFloat yOriginOfBottomView = CGRectGetMaxY(self.topView.frame) + width;
+    CGFloat heightOfBottomView = CGRectGetHeight(self.view.frame) - yOriginOfBottomView;
+    self.bottomView.frame = CGRectMake(0, yOriginOfBottomView, width, heightOfBottomView);
+    
+    CGFloat cameraToolbarHeight = 100;
+    self.cameraToolbar.frame = CGRectMake(0, CGRectGetHeight(self.view.bounds) - cameraToolbarHeight, width, cameraToolbarHeight);
+}
+
+- (void) cropPressed:(UIBarButtonItem *)sender {
+    CGRect visibleRect;
+    float scale = 1.0f / self.scrollView.zoomScale / self.media.image.scale;
+    visibleRect.origin.x = self.scrollView.contentOffset.x * scale;
+    visibleRect.origin.y = self.scrollView.contentOffset.y * scale;
+    visibleRect.size.width = self.scrollView.bounds.size.width * scale;
+    visibleRect.size.height = self.scrollView.bounds.size.height * scale;
+    
+    UIImage *scrollViewCrop = [self.media.image imageWithFixedOrientation];
+    scrollViewCrop = [scrollViewCrop imageCroppedToRect:visibleRect];
+    
+    [self.delegate cropControllerFinishedWithImage:scrollViewCrop];
+}
+
+- (void) leftButtonPressedOnToolbar:(CameraToolbar *)toolbar{
+}
+
+- (void) rightButtonPressedOnToolbar:(CameraToolbar *)toolbar{
+}
+
+- (void) cameraButtonPressedOnToolbar:(CameraToolbar *)toolbar{
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
